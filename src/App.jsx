80c4bd932366e3612d38c2a757d2efa843fea6ed
Andrew <149857208+PrimeBeyonder@users.noreply.github.com>
2024-11-27 21:52:01 +0630

    hero sections, btn and preview components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+
+import Hero from './components/Hero'
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Hero/>
+    </main>
+    </>
   )
 }
 
--- src/components/VideoPreview.jsx
+++ src/components/VideoPreview.jsx
@@ -1,11 +1,78 @@
-import React from 'react'
+import { gsap } from "gsap";
+import { useState, useRef, useEffect } from "react";
 
-const VideoPreview = () => {
+// eslint-disable-next-line react/prop-types
+export const VideoPreview = ({ children }) => {
+  const [isHovering, setIsHovering] = useState(false);
+  const sectionRef = useRef(null);
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.set(contentRef.current, {
+        transformStyle: "preserve-3d",
+        transformPerspective: 1000,
+      });
+    }, sectionRef);
+
+    return () => ctx.revert();
+  }, []);
+
+  const handleMouseMove = ({ clientX, clientY, currentTarget }) => {
+    if (!isHovering) return;
+
+    const rect = currentTarget.getBoundingClientRect();
+    const xOffset = (clientX - (rect.left + rect.width / 2)) / 10;
+    const yOffset = (clientY - (rect.top + rect.height / 2)) / 10;
+
+    gsap.to(contentRef.current, {
+      rotationY: xOffset,
+      rotationX: -yOffset,
+      x: xOffset * 2,
+      y: yOffset * 2,
+      z: 30,
+      duration: 0.5,
+      ease: "power2.out",
+    });
+  };
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+    gsap.to(contentRef.current, {
+      scale: 1.05,
+      duration: 0.3,
+      ease: "power2.out",
+    });
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+    gsap.to(contentRef.current, {
+      rotationY: 0,
+      rotationX: 0,
+      x: 0,
+      y: 0,
+      z: 0,
+      scale: 1,
+      duration: 0.5,
+      ease: "power2.out",
+    });
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section
+      ref={sectionRef}
+      onMouseMove={handleMouseMove}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className="absolute z-50  size-full overflow-hidden rounded-lg"
+    >
+      <div ref={contentRef} className="size-full  origin-center rounded-lg">
+        {children}
+      </div>
+    </section>
+  );
+};
 
-export default VideoPreview
+export default VideoPreview;
+
